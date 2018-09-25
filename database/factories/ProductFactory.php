20172dc710bcf3ff08f77a770a935f86a41aa68f
Gustavo Vasquez <gustavoalejandro@hotmail.cl>
2018-09-25 00:07:01 -0300

    Laravel Ajax

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->sentence(),
+        'short' => $faker->text(140),
+        'description' => $faker->text(600)
+        
+    ];
+});
